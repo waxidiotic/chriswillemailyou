b39bd569da3bac329c4bbd36557e733fad24d7e3
Alex Bussey <awbussey83@gmail.com>
2021-01-22 18:53:54 -0500

    Data yay

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,6 @@ interface AppProps {
 }
 
 function App({ sb }: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-
-  useEffect(() => {
-    async function getData() {
-      const { data: people } = await sb.from('people').select('name');
-      console.log(people);
-    }
-
-    getData();
-  }, []);
-
   return (
     <div className={css.layout}>
       <div className={css.header}>
@@ -37,7 +19,7 @@ function App({ sb }: AppProps) {
       </div>
       <div className={css.content}>
         <TableControls />
-        <Table />
+        <Table sb={sb} />
       </div>
       <div className={css.footer}>Footer</div>
     </div>
--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,29 +1,81 @@
 import React from 'react';
 import { Table as AntTable } from 'antd';
+import type { ColumnsType } from 'antd/lib/table';
+import type { SupabaseClient } from '@supabase/supabase-js';
 
-const columns = [
+interface Check {
+  key: string; // uuid
+  venue: string;
+  people: string[];
+  total?: number;
+  items?: any;
+  paid: boolean;
+}
+
+const columns: ColumnsType<Check> = [
   {
+    key: 'date',
     title: 'Date',
     dataIndex: 'date',
   },
   {
+    key: 'venue',
     title: 'Venue',
     dataIndex: 'venue',
   },
   {
+    key: 'people',
     title: 'People',
     dataIndex: 'people',
   },
   {
+    key: 'total',
     title: 'Total',
     dataIndex: 'total',
   },
   {
+    key: 'paid',
     title: 'Paid',
     dataIndex: 'paid',
   },
 ];
 
-export default function Table() {
-  return <AntTable columns={columns} dataSource={undefined} />;
+export default function Table({ sb }: { sb: SupabaseClient }) {
+  const [data, setData] = React.useState<Check[]>([]);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    async function fetchChecks() {
+      setIsLoading(true);
+      const { data: checks } = await sb.from('checks').select('*');
+
+      if (checks) {
+        const mappedData = checks.map((check) => {
+          const { id, vendor, people, items, total, paid } = check;
+          return {
+            key: id,
+            venue: vendor,
+            people,
+            items,
+            total,
+            paid,
+          };
+        });
+        setData(mappedData);
+      }
+    }
+
+    fetchChecks().then(() => {
+      setIsLoading(false);
+    });
+  }, []);
+
+  return (
+    <AntTable<Check>
+      columns={columns}
+      rowKey={(record) => record.key}
+      dataSource={data}
+      loading={isLoading}
+    />
+  );
 }
